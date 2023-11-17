025cdfa3fadedbf4c6c4602cb4b3960b4d314b7c
ShirazAkram <mdshirazakram2326@gmail.com>
2023-11-17 20:19:17 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+const port = 8080
+app.listen(port, () => {
+    console.log("App listen on" , port)
+})
+
+app.get("/home", (req, resp) => {
+    console.log("Browser requested")
+    resp.send("<h1>Hello Browser</h1>")
+})
