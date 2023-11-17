39d0c45912003d3a1cf49d8bdee7b34c72fcd8b5
ShirazAkram <mdshirazakram2326@gmail.com>
2023-11-17 20:25:11 +0530

    added post request

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ app.get("/home", (req, resp) => {
     console.log("Browser requested")
     resp.send("<h1>Hello Browser</h1>")
 })
+
+app.post("/home", (req, resp) => {
+    console.log("Browser requested" , "POST API")
+    resp.send("Hello Request")
+})
